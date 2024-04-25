543e3ac0c45d1094da8cbe0515e33bc32361a1bd
sncozzi <santiago.cozzi@anima.edu.uy>
2024-04-25 20:12:58 -0300

    orderRoutes update

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const { Order } = require("../models/index");
 const orderController = require("../controllers/orderController");
 const isAdmin = require("../middlewares/isAdmin");
+const { expressjwt: checkJwt } = require("express-jwt");
 
 
 router.get("/", orderController.index);
 router.get("/:id", orderController.show);
 router.post("/",  orderController.store);
 router.patch("/:id", orderController.update);
-router.delete("/:id", isAdmin, orderController.destroy);
+router.delete("/:id", checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"]}), isAdmin, orderController.destroy);
 
 module.exports = router;
