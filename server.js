06c0ed0a2d20bfb0224331baa7a8b2823ebea07c
sncozzi <santiago.cozzi@anima.edu.uy>
2024-06-04 20:51:00 -0300

    cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}.\n`)
