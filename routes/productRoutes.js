c43b2eaab5dd484e05d508091ba295b11bb50474
bahiyyihteamo <luana.cruz@anima.edu.uy>
2024-07-01 20:51:40 -0300

    admin dashboard style

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,11 +5,20 @@ const productController = require("../controllers/productController");
 const isAdmin = require("../middlewares/isAdmin");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-
 router.get("/", productController.index);
 router.get("/:id", productController.show);
-router.post("/", checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"]}), isAdmin, productController.store);
-router.patch("/:id", checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"]}),isAdmin, productController.update);
-router.delete("/:id", checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"]}), isAdmin, productController.destroy);
+router.post(
+  "/",
+  checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"] }),
+  isAdmin,
+  productController.store
+);
+router.patch("/:id", productController.update);
+router.delete(
+  "/:id",
+  checkJwt({ secret: process.env.SECRET_TOKEN, algorithms: ["HS256"] }),
+  isAdmin,
+  productController.destroy
+);
 
 module.exports = router;
